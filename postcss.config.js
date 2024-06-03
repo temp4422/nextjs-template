0cc690dfc00fb58d301eeaa1be3a1ffeafecc09a
user <user@MacBook-Pro-user.local>
2024-06-03 15:08:40 +0300

    Update next.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// }
+// export default config
+
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
+    // autoprefixer: {},
   },
 }
